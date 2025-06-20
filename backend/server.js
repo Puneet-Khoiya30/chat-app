cda5210e451b1cc458ff447a2c80ebe298e0839a
Puneet Khoiya <puneetkhoiyabnl8@gmail.com>
2025-06-21 00:46:36 +0530

    env fixed

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import { app, server } from "./socket/socket.js";
 import dotenv from 'dotenv';
 dotenv.config();
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 // PORT should be assigned after calling dotenv.config() because we need to access the env variables. Didn't realize while recording the video. Sorry for the confusion.
 const PORT = process.env.PORT || 5000;
 
@@ -24,11 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-});
+// app.get("*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
 
 server.listen(PORT, () => {
 	connectToMongoDB();
