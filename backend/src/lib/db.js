99df05b5f9c2dd97db6f55f495cf0cd02f882228
Puneet Khoiya <puneetkhoiyabnl8@gmail.com>
2025-08-07 20:15:52 +0530

    fixed errors

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -3,7 +3,10 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
   try {
     console.log("MONGODB_URI:", process.env.MONGODB_URI);
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
