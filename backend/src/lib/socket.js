dff1bfbf004cf420db98bad5f806040883dda692
Puneet Khoiya <puneetkhoiyabnl8@gmail.com>
2025-08-07 19:46:21 +0530

    fixed errors

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,9 +5,11 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+const frontendUrl = process.env.NODE_ENV === "production" ? "https://chat-with-pk.vercel.app" : "localhost:5173";
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [frontendUrl],
   },
 });
 
